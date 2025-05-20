406ad187cb6136880c4058e6a77192ff3da361de
Isaac-Lima <isaac.lima25@aluno.ce.gov.br>
2025-05-20 15:19:08 -0300

    :sparkles: feat: criado models

---
+++ Locadora/Data/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Locadora.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Locadora.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+            
+        }
+
+        // Criação de tabelas
+        public DbSet<AutorModel> Autores { get; set; }
+        public DbSet<LivroModel> Livros { get; set; }
+         
+    }
+}
---
+++ Locadora/Models/AutorModel.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Locadora.Models
+{
+    public class AutorModel
+    {
+        public int Id { get; set; } 
+        public string Nome { get; set; } 
+        public string Sobrenome { get; set; }
+        [JsonIgnore] // Não aparece a lista de livros ao criar um autor
+        public ICollection<LivroModel> Livros { get; set; }
+    }
+}
